7c2e28439b1391dc9f2f7d634ab91896cfa650d9
fyang <fyangami@163.com>
2024-11-25 05:41:44 +0000

    Enhance Todo List query with parent_id handling

--- internal/services/todo.go
+++ internal/services/todo.go
@@ -42,6 +42,7 @@ func (t *TodoService) Get(querier *models.TodoQuerier) (*models.Todo, error) {
 func (*TodoService) List(querier *models.TodoQuerier) ([]*models.Todo, error) {
 	var list []*models.Todo
 	querierMap := map[string]interface{}{}
+	sql := globals.DB.Model(&models.Todo{})
 	if querier.ID != nil {
 		querierMap["id"] = querier.ID
 	}
@@ -53,8 +54,10 @@ func (*TodoService) List(querier *models.TodoQuerier) ([]*models.Todo, error) {
 	}
 	if querier.ParentID != nil {
 		querierMap["parent_id"] = querier.ParentID
+	} else {
+		sql = sql.Where("parent_id is null")
 	}
-	sql := globals.DB.Model(&models.Todo{}).Where(querierMap)
+	sql = sql.Where(querierMap)
 	if querier.TimeRange != nil {
 		sql = querier.TimeRange.RangeSql(sql, "deadline")
 	}
