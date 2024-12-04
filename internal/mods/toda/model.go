616432f8096183e3584f2853296eba39cbc4396d
fyang <fyangami@163.com>
2024-12-04 12:55:33 +0000

    update model

--- internal/mods/toda/model.go
+++ internal/mods/toda/model.go
@@ -1,32 +1,33 @@
 package toda
 
 import (
-	"time"
 	"dailydo.fe1.xyz/internal/common"
+	"time"
 )
 
 type Toda struct {
-	common.BaseModel  
-	Title string `json:"title" ` 
-	Description string `json:"description" ` 
-	UserId uint `json:"userId" gorm:"index"` 
-	Priority int `json:"priority" ` 
-	Deadline *time.Time `json:"deadline" ` 
-	Status int `json:"status" ` 
-	Estimate int `json:"estimate" ` 
-	Elapsed int `json:"elapsed" `
+	common.BaseModel
+	Title       string     `json:"title" `
+	Description string     `json:"description" `
+	UserId      uint       `json:"userId" gorm:"index"`
+	Priority    int        `json:"priority" `
+	Deadline    *time.Time `json:"deadline" `
+	Status      int        `json:"status" `
+	Estimate    int        `json:"estimate" `
+	Elapsed     int        `json:"elapsed" `
 }
 
 type TodaQuerier struct {
-	common.Pager 
-	Title string `json:"title"` 
-	Description string `json:"description"` 
-	UserId uint `json:"userId"` 
-	Priority int `json:"priority"` 
-	Deadline time.Time `json:"deadline"` 
-	Status int `json:"status"` 
-	Estimate int `json:"estimate"` 
-	Elapsed int `json:"elapsed"` 
+	common.Pager
+	Id          *uint      `json:"id"`
+	Title       *string    `json:"title"`
+	Description *string    `json:"description"`
+	UserId      *uint      `json:"userId"`
+	Priority    *int       `json:"priority"`
+	Deadline    *time.Time `json:"deadline"`
+	Status      *int       `json:"status"`
+	Estimate    *int       `json:"estimate"`
+	Elapsed     *int       `json:"elapsed"`
 }
 
 const (
